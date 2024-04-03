7337081455e5fd86e2579e1fd4f522bc7ce4369a
Aleh Kastsiukovich <alehkastsiukovich@Alehs-MacBook-Pro.local>
2024-04-03 14:56:02 +0200

    initial commit

---
+++ composeApp/src/commonMain/kotlin/ui/App.kt
@@ -0,0 +1,21 @@
+package ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        }
+    }
+}
